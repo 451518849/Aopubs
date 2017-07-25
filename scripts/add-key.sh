ac6fb8a490699cb8d31b39939f17aac720d6fcfe
wangxiaofa <wangxiaofa@corp-ci.com>
2017-07-25 16:39:18 +0800

    udpate

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -4,8 +4,8 @@ security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 security import ./scripts/certs/ios_development.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./scripts/certs/certs.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./scripts/profile/patient_adolescent_spin_distribute.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./scripts/profile/app_for_adolescent_spine.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 security find-identity -p codesigning ~/Library/Keychains/ios-build.keychain
 security list-keychain
--- scripts/remove-key.sh
+++ scripts/remove-key.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 security delete-keychain ios-build.keychain
-rm -f ~/Library/MobileDevice/Provisioning\ Profiles/patient_adolescent_spin_distribute.mobileprovision
+rm -f ~/Library/MobileDevice/Provisioning\ Profiles/app_for_adolescent_spine.mobileprovision
